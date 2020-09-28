f72175f19e159ff93b9dd41c19df7d49df90e4da
Hezekiah Branch <hezekiah.branch@tufts.edu>
2020-09-28 15:20:15 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="readin-ucinet-hezbranch", # Replace with your own username
+    version="1.0.0",
+    author="Hezekiah Branch",
+    author_email="hezekiah.branch.tufts@gmail.com",
+    description="Read in UCINET file as pandas DF",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
