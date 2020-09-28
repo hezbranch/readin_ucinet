bfbe97f2450f52e937ca362306095ba281eb707c
Hezekiah Branch <hezekiah.branch@tufts.edu>
2020-09-28 17:10:00 -0400

    Latest release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="readin_ucinet",
-    version="1.3.2",
+    version="1.3.3",
     author="Hezekiah Branch",
     author_email="hezekiah.branch.tufts@gmail.com",
     description="Read in UCINET file as pandas DF",
