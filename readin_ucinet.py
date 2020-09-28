e2c0a24ccc0df313d72412158a60fd3260842181
Hezekiah Branch <hezekiah.branch@tufts.edu>
2020-09-28 16:42:15 -0400

    Update readin_ucinet.py

--- readin_ucinet.py
+++ readin_ucinet.py
@@ -6,18 +6,19 @@
 import os
 import pandas as pd
 
-# First read in UCINET DL dataset
-ucinet_filepath = input("Enter path of dataset: ")
-os.chdir(ucinet_filepath)
-ucinet_file = open(input("Enter name of file: "), "r")
-ucinet_graph = []
-for line in ucinet_file.readlines():
-        if (line[0].isalpha() == False):
-            add_row = []
-            for edge in line:
-                if (edge >= '0'):
-                    add_row.append(int(edge))
-            ucinet_graph.append(add_row)
-ucinet_file.close()
-pd_ucinet_graph = pd.DataFrame(data=ucinet_graph)
-print(pd_ucinet_graph)
+def read_ucinet(ucinet_filepath):
+    # First read in UCINET DL dataset
+    os.chdir(ucinet_filepath)
+    ucinet_file = open(filepath, "r")
+    ucinet_graph = []
+    for line in ucinet_file.readlines():
+            if (line[0].isalpha() == False):
+                add_row = []
+                for edge in line:
+                    if (edge >= '0'):
+                        add_row.append(int(edge))
+                ucinet_graph.append(add_row)
+    ucinet_file.close()
+    pd_ucinet_graph = pd.DataFrame(data=ucinet_graph)
+    print(pd_ucinet_graph)
+    return ucinet_graph
