412a3d7fbac1e4c7dc70c2e371b436a120ab0e9e
Hezekiah Branch <hezekiah.branch@tufts.edu>
2020-09-28 15:21:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Read in UCINET file as pandas DF",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/hezbranch/readin_ucinet",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
